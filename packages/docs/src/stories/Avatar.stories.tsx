6d031f2e642ef8581efc14076042646b08f1c20c
Nathan Baldez <nathanbsb2@hotmail.com>
2024-01-18 09:45:58 -0300

    :tada: Components controls :tada:

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -9,6 +9,13 @@ export default {
     src: 'https://github.com/nthbaldez.png',
     alt: 'Nathan Baldez',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
