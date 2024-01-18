fa78c23d015caed84c38d77850732111369ff59d
Nathan Baldez <nathanbsb2@hotmail.com>
2024-01-18 10:20:17 -0300

    :fix: add subpath to storybook prd :fix:

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -25,5 +25,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config,{ configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/design-system/'
+    }
+
+    return config
+  },
 };
 export default config;
