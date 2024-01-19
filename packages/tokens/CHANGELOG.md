a6db83b2eacd1713044e00e5e17cb5dcb0766a53
Nathan Baldez <nathanbsb2@hotmail.com>
2024-01-19 16:30:15 -0300

    :tada: ci cd test :tada:

--- packages/tokens/CHANGELOG.md
+++ packages/tokens/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @nth-uidesign/tokens
 
+## 3.0.0
+
+### Major Changes
+
+- CI/CD test
+
 ## 2.0.0
 
 ### Major Changes
--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -16,4 +16,5 @@ export const colors = {
   ignite500: '#00875F',
   ignite700: '#015F43',
   ignite900: '#00291D',
+  test: '#fff',
 }
